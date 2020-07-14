6ea6e8a32846a4827e6f45ac8f62d33cd2cda313
siesse.jordan <siessejordan@gmail.com>
2020-07-14 17:57:06 -0500

    additions to SuperSightingsDB and DBTest

---
+++ SuperSightingsDB.sql
@@ -0,0 +1,46 @@
+DROP DATABASE IF EXISTS SuperSightingsDB;
+CREATE DATABASE SuperSightingsDB;
+
+USE SuperSightingsDB;
+
+create table supers(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    description varchar(255),
+    power varchar(30) NOT NULL
+);
+
+create table organizations(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    orgName varchar(80),
+    orgDescription varchar(255),
+    address varchar(100),
+    phone varchar(20)
+);
+
+create table superOrganizations(
+    superId INT NOT NULL,
+    orgId INT NOT NULL,
+    
+    FOREIGN KEY (superId) REFERENCES supers(id),
+    FOREIGN KEY (orgId) REFERENCES organizations(id)
+);
+
+create table locations(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    locationName varchar(50),
+	description varchar(255),
+    address varchar(100),
+    latitude INT NOT NULL,
+    longitude INT NOT NULL
+);
+
+create table sightings(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    locationId INT NOT NULL,
+    superId INT NOT NULL,
+    `date` date NOT NULL,
+    
+    FOREIGN KEY (locationId) REFERENCES locations(id),
+    FOREIGN KEY (superId) REFERENCES supers(id)
+);
---
+++ supersightings/src/main/java/com/sg/supersightings/models/User.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.supersightings.models;
+
+import java.util.Objects;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author siessejordan
+ */
+@Entity
+public class User {
+    
+    private int id;
+    private String userName;
+    private String password;
+    private boolean enabled;
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + this.id;
+        hash = 53 * hash + Objects.hashCode(this.userName);
+        hash = 53 * hash + Objects.hashCode(this.password);
+        hash = 53 * hash + (this.enabled ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.enabled != other.enabled) {
+            return false;
+        }
+        if (!Objects.equals(this.userName, other.userName)) {
+            return false;
+        }
+        if (!Objects.equals(this.password, other.password)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the enabled
+     */
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    /**
+     * @param enabled the enabled to set
+     */
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+    
+}
